e977bb7d8f6183c71fdbc66c51b9fe3ccddeb532
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2017-12-26 11:52:54 -0500

    Alter default benchmarking configuration.

--- test/unir_SUITE.erl
+++ test/unir_SUITE.erl
@@ -1169,7 +1169,7 @@ rand_bits(Bits) ->
 
 %% @private
 default_bench_configuration() ->
-    [{bench_config, "unir_basic.config"}].
+    [{bench_config, "unir_1kb_object.config"}].
 
 %% @private
 bench_config() ->
