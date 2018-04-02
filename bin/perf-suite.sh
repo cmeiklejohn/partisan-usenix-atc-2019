aacb34fad1fbb5687b19b5f395bfeb16a95c6371
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2018-04-02 05:15:47 -0400

    Add perf suite.

--- bin/perf-suite.sh
+++ bin/perf-suite.sh
@@ -1,44 +1,46 @@
 #!/bin/bash
 
+# Other options: make partisan-perf
+
 sudo tc qdisc del dev lo root netem
 
 # 1ms RTT
-sudo tc qdisc add dev lo root netem delay 0.5ms
-
-LATENCY=1 SIZE=1024 CONCURRENCY=1 make echo-perf
-LATENCY=1 SIZE=1024 CONCURRENCY=2 make echo-perf
-LATENCY=1 SIZE=1024 CONCURRENCY=4 make echo-perf
-LATENCY=1 SIZE=1024 CONCURRENCY=8 make echo-perf
-
-LATENCY=1 SIZE=2048 CONCURRENCY=1 make echo-perf
-LATENCY=1 SIZE=2048 CONCURRENCY=2 make echo-perf
-LATENCY=1 SIZE=2048 CONCURRENCY=4 make echo-perf
-LATENCY=1 SIZE=2048 CONCURRENCY=8 make echo-perf
-
-LATENCY=1 SIZE=4096 CONCURRENCY=1 make echo-perf
-LATENCY=1 SIZE=4096 CONCURRENCY=2 make echo-perf
-LATENCY=1 SIZE=4096 CONCURRENCY=4 make echo-perf
-LATENCY=1 SIZE=4096 CONCURRENCY=8 make echo-perf
-
-LATENCY=1 SIZE=8192 CONCURRENCY=1 make echo-perf
-LATENCY=1 SIZE=8192 CONCURRENCY=2 make echo-perf
-LATENCY=1 SIZE=8192 CONCURRENCY=4 make echo-perf
-LATENCY=1 SIZE=8192 CONCURRENCY=8 make echo-perf
-
-sudo tc qdisc del dev lo root netem
+#sudo tc qdisc add dev lo root netem delay 0.5ms
+#
+#LATENCY=1 SIZE=1024 CONCURRENCY=1 make echo-perf
+#LATENCY=1 SIZE=1024 CONCURRENCY=2 make echo-perf
+#LATENCY=1 SIZE=1024 CONCURRENCY=4 make echo-perf
+#LATENCY=1 SIZE=1024 CONCURRENCY=8 make echo-perf
+#
+#LATENCY=1 SIZE=2048 CONCURRENCY=1 make echo-perf
+#LATENCY=1 SIZE=2048 CONCURRENCY=2 make echo-perf
+#LATENCY=1 SIZE=2048 CONCURRENCY=4 make echo-perf
+#LATENCY=1 SIZE=2048 CONCURRENCY=8 make echo-perf
+#
+#LATENCY=1 SIZE=4096 CONCURRENCY=1 make echo-perf
+#LATENCY=1 SIZE=4096 CONCURRENCY=2 make echo-perf
+#LATENCY=1 SIZE=4096 CONCURRENCY=4 make echo-perf
+#LATENCY=1 SIZE=4096 CONCURRENCY=8 make echo-perf
+#
+#LATENCY=1 SIZE=8192 CONCURRENCY=1 make echo-perf
+#LATENCY=1 SIZE=8192 CONCURRENCY=2 make echo-perf
+#LATENCY=1 SIZE=8192 CONCURRENCY=4 make echo-perf
+#LATENCY=1 SIZE=8192 CONCURRENCY=8 make echo-perf
+#
+#sudo tc qdisc del dev lo root netem
 
 # 20ms RTT
 sudo tc qdisc add dev lo root netem delay 10ms
 
-LATENCY=20 SIZE=1024 CONCURRENCY=1 make echo-perf
-LATENCY=20 SIZE=1024 CONCURRENCY=2 make echo-perf
-LATENCY=20 SIZE=1024 CONCURRENCY=4 make echo-perf
-LATENCY=20 SIZE=1024 CONCURRENCY=8 make echo-perf
+#LATENCY=20 SIZE=1024 CONCURRENCY=1 make echo-perf
+#LATENCY=20 SIZE=1024 CONCURRENCY=2 make echo-perf
+#LATENCY=20 SIZE=1024 CONCURRENCY=4 make echo-perf
+#LATENCY=20 SIZE=1024 CONCURRENCY=8 make echo-perf
 
-LATENCY=20 SIZE=2048 CONCURRENCY=1 make echo-perf
-LATENCY=20 SIZE=2048 CONCURRENCY=2 make echo-perf
-LATENCY=20 SIZE=2048 CONCURRENCY=4 make echo-perf
-LATENCY=20 SIZE=2048 CONCURRENCY=8 make echo-perf
+#LATENCY=20 SIZE=2048 CONCURRENCY=1 make echo-perf
+#LATENCY=20 SIZE=2048 CONCURRENCY=2 make echo-perf
+#LATENCY=20 SIZE=2048 CONCURRENCY=4 make echo-perf
+#LATENCY=20 SIZE=2048 CONCURRENCY=8 make echo-perf
 
 LATENCY=20 SIZE=4096 CONCURRENCY=1 make echo-perf
 LATENCY=20 SIZE=4096 CONCURRENCY=2 make echo-perf
