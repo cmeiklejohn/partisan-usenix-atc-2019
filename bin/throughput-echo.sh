c9f0c34a5b0e0a97f0cd026b38ca1e1697c3b4b1
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2019-01-02 19:25:43 -0500

    New experiments.

--- bin/throughput-echo.sh
+++ bin/throughput-echo.sh
@@ -5,6 +5,15 @@ sudo tc qdisc del dev lo root netem
 # 1ms RTT
 sudo tc qdisc add dev lo root netem delay 0.5ms
 
+LATENCY=1 SIZE=512 CONCURRENCY=1 make echo-perf
+LATENCY=1 SIZE=512 CONCURRENCY=2 make echo-perf
+LATENCY=1 SIZE=512 CONCURRENCY=4 make echo-perf
+LATENCY=1 SIZE=512 CONCURRENCY=8 make echo-perf
+LATENCY=1 SIZE=512 CONCURRENCY=16 make echo-perf
+LATENCY=1 SIZE=512 CONCURRENCY=32 make echo-perf
+LATENCY=1 SIZE=512 CONCURRENCY=64 make echo-perf
+LATENCY=1 SIZE=512 CONCURRENCY=128 make echo-perf
+
 LATENCY=1 SIZE=1024 CONCURRENCY=1 make echo-perf
 LATENCY=1 SIZE=1024 CONCURRENCY=2 make echo-perf
 LATENCY=1 SIZE=1024 CONCURRENCY=4 make echo-perf
@@ -27,6 +36,15 @@ sudo tc qdisc del dev lo root netem
 
 # 20ms RTT
 sudo tc qdisc add dev lo root netem delay 10ms
+
+LATENCY=20 SIZE=512 CONCURRENCY=1 make echo-perf
+LATENCY=20 SIZE=512 CONCURRENCY=2 make echo-perf
+LATENCY=20 SIZE=512 CONCURRENCY=4 make echo-perf
+LATENCY=20 SIZE=512 CONCURRENCY=8 make echo-perf
+LATENCY=20 SIZE=512 CONCURRENCY=16 make echo-perf
+LATENCY=20 SIZE=512 CONCURRENCY=32 make echo-perf
+LATENCY=20 SIZE=512 CONCURRENCY=64 make echo-perf
+LATENCY=20 SIZE=512 CONCURRENCY=128 make echo-perf
 
 LATENCY=20 SIZE=1024 CONCURRENCY=1 make echo-perf
 LATENCY=20 SIZE=1024 CONCURRENCY=2 make echo-perf
