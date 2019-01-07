e6d95767cd4384afe287a6a7a9be97193de04b24
Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>
2019-01-07 13:47:59 -0500

    Add new configuration.

--- bin/microbenchmarks-queueoverhead.sh
+++ bin/microbenchmarks-queueoverhead.sh
@@ -6,12 +6,6 @@ sudo tc qdisc del dev lo root netem
 # 1ms RTT
 sudo tc qdisc add dev lo root netem delay 10ms
 
-LATENCY=20 SIZE=8192 CONCURRENCY=2 make microbenchmarks-queueoverhead
-LATENCY=20 SIZE=8192 CONCURRENCY=4 make microbenchmarks-queueoverhead
-LATENCY=20 SIZE=8192 CONCURRENCY=8 make microbenchmarks-queueoverhead
-LATENCY=20 SIZE=8192 CONCURRENCY=16 make microbenchmarks-largepayload
-LATENCY=20 SIZE=8192 CONCURRENCY=32 make microbenchmarks-queueoverhead
-LATENCY=20 SIZE=8192 CONCURRENCY=64 make microbenchmarks-queueoverhead
 LATENCY=20 SIZE=8192 CONCURRENCY=128 make microbenchmarks-queueoverhead
 
 sudo tc qdisc del dev lo root netem
