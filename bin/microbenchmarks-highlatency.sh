c8fb5cd7a613bba2d77aa8c90c18d8b766e70a84
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2019-06-02 18:19:32 +0000

    Update microbenchmark configuration.

--- bin/microbenchmarks-highlatency.sh
+++ bin/microbenchmarks-highlatency.sh
@@ -6,9 +6,6 @@ sudo tc qdisc del dev lo root netem
 # 1ms RTT
 sudo tc qdisc add dev lo root netem delay 10ms
 
-LATENCY=20 SIZE=512 CONCURRENCY=2 make microbenchmarks-highlatency
-LATENCY=20 SIZE=512 CONCURRENCY=4 make microbenchmarks-highlatency
-LATENCY=20 SIZE=512 CONCURRENCY=8 make microbenchmarks-highlatency
 LATENCY=20 SIZE=512 CONCURRENCY=16 make microbenchmarks-highlatency
 LATENCY=20 SIZE=512 CONCURRENCY=32 make microbenchmarks-highlatency  
 LATENCY=20 SIZE=512 CONCURRENCY=64 make microbenchmarks-highlatency
--- bin/microbenchmarks.sh
+++ bin/microbenchmarks.sh
@@ -6,9 +6,6 @@ sudo tc qdisc del dev lo root netem
 # 1ms RTT
 sudo tc qdisc add dev lo root netem delay 0.5ms
 
-LATENCY=1 SIZE=512 CONCURRENCY=2 make microbenchmarks
-LATENCY=1 SIZE=512 CONCURRENCY=4 make microbenchmarks
-LATENCY=1 SIZE=512 CONCURRENCY=8 make microbenchmarks
 LATENCY=1 SIZE=512 CONCURRENCY=16 make microbenchmarks
 LATENCY=1 SIZE=512 CONCURRENCY=32 make microbenchmarks
 LATENCY=1 SIZE=512 CONCURRENCY=64 make microbenchmarks
